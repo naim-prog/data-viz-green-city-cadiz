5d3ff01bc5c5780de33c3e66625d3e60580af421
Naím Rodríguez <naimrodrey@proton.me>
2026-04-12 18:00:18 +0200

    Fix: URL corregido

--- docs/script.js
+++ docs/script.js
@@ -21,7 +21,7 @@ function obtenerColor(porcentaje) {
 }
 
 // 4. Leemos el CSV desde GitHub Pages
-Papa.parse("https://github.com/naim-prog/data-viz-green-city-cadiz/blob/main/files/datos_municipios.csv", {
+Papa.parse("https://raw.githubusercontent.com/naim-prog/data-viz-green-city-cadiz/refs/heads/main/files/datos_municipios.csv", {
     download: true,       // Le dice a PapaParse que es un archivo externo
     header: true,         // Usa la primera fila como nombres de variables
     dynamicTyping: true,  // Convierte los números de texto a formato numérico
@@ -41,7 +41,7 @@ Papa.parse("https://github.com/naim-prog/data-viz-green-city-cadiz/blob/main/fil
 
 // 5. Función para cargar dibujar los municipios
 function cargarGeoJSON() {
-    fetch('https://github.com/naim-prog/data-viz-green-city-cadiz/blob/main/files/municipios_cadiz.geojson') // Tu archivo con las fronteras
+    fetch('https://raw.githubusercontent.com/naim-prog/data-viz-green-city-cadiz/refs/heads/main/files/municipios_cadiz.geojson') // Tu archivo con las fronteras
         .then(respuesta => respuesta.json())
         .then(datosGeoJSON => {
             
